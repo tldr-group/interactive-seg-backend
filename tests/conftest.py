b9c599ce59e5199889c2c2d902994c56780f3395
rmdocherty <rmdocherty2@gmai.com>
2025-02-25 19:11:27 +0000

    moved shared objects (feat stacks, image) into conftest

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from interactive_seg_backend.configs import Arr, FeatureConfig
+from interactive_seg_backend.features import multiscale_features
+from interactive_seg_backend.file_handling import (
+    load_image,
+    load_labels,
+)
+
+
+@pytest.fixture(scope="module")
+def image() -> Arr:
+    return load_image("tests/data/0.tif")
+
+
+@pytest.fixture(scope="module")
+def labels() -> Arr:
+    return load_labels("tests/data/0_labels.tif")
+
+
+@pytest.fixture(scope="module")
+def feat_cfg() -> FeatureConfig:
+    return FeatureConfig(add_weka_sigma_multiplier=False)
+
+
+@pytest.fixture(scope="module")
+def feature_stack(image: Arr, feat_cfg: FeatureConfig) -> Arr:
+    return multiscale_features(image, feat_cfg)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,7 @@
 import pytest
 
 from interactive_seg_backend.configs import Arr, UInt8Arr, FeatureConfig, TrainingConfig
-from interactive_seg_backend.features import multiscale_features
 from interactive_seg_backend.file_handling import (
-    load_image,
     load_labels,
     save_segmentation,
 )
@@ -20,32 +18,12 @@ from interactive_seg_backend.utils import class_avg_miou
 TEST_IMAGE_SHAPE = (512, 512)
 
 
-@pytest.fixture
-def image() -> Arr:
-    return load_image("tests/data/0.tif")
-
-
-@pytest.fixture
-def labels() -> Arr:
-    return load_labels("tests/data/0_labels.tif")
-
-
 def test_load_image(image: Arr) -> None:
     assert image.shape == TEST_IMAGE_SHAPE
 
 
 def test_load_labels(labels: UInt8Arr) -> None:
     assert labels.shape == TEST_IMAGE_SHAPE
-
-
-@pytest.fixture
-def feat_cfg() -> FeatureConfig:
-    return FeatureConfig(add_weka_sigma_multiplier=False)
-
-
-@pytest.fixture
-def feature_stack(image: Arr, feat_cfg: FeatureConfig) -> Arr:
-    return multiscale_features(image, feat_cfg)
 
 
 def test_feat_stack(feat_cfg: FeatureConfig, feature_stack: Arr):
@@ -63,7 +41,12 @@ def train_cfg(feat_cfg: FeatureConfig) -> TrainingConfig:
 MIOU_CUTOFF = 0.8
 
 
-def test_e2e(feature_stack: Arr, labels: UInt8Arr, train_cfg: TrainingConfig):
+def test_e2e(
+    feature_stack: Arr,
+    labels: UInt8Arr,
+    train_cfg: TrainingConfig,
+    out_fname: str = "tests/out/0_seg.tif",
+):
     fit, target = get_labelled_training_data_from_stack(feature_stack, labels)
     fit, target = shuffle_sample_training_data(
         fit, target, train_cfg.shuffle_data, train_cfg.n_samples
@@ -74,7 +57,7 @@ def test_e2e(feature_stack: Arr, labels: UInt8Arr, train_cfg: TrainingConfig):
     rh, rw = pred.shape
     fh, fw, _ = feature_stack.shape
 
-    save_segmentation(pred, "tests/out/0_seg.tif")
+    save_segmentation(pred, out_fname)
 
     assert rh == fh
     assert rw == fw
