2a7032e402fd082a9d99695cb670d521075f3e51
rmdocherty <rmdocherty2@gmai.com>
2025-02-25 19:10:38 +0000

    revert abstract class stuff

--- interactive_seg_backend/classifiers/base.py
+++ interactive_seg_backend/classifiers/base.py
@@ -3,29 +3,24 @@ from pickle import load, dump
 from skops.io import load as skload, dump as skdump
 
 
-from abc import ABCMeta, abstractmethod
 from typing import Any
 
 from interactive_seg_backend.configs import Arr, UInt8Arr
 
 
-class Classifier(object, metaclass=ABCMeta):
-    @abstractmethod
+class Classifier(object):
     def __init__(self, extra_args: dict[str, Any]) -> None:
         pass
 
-    @abstractmethod
     def fit(
         self, train_data: Arr, target_data: UInt8Arr, sample_weights: Arr | None = None
     ):
         return self
 
-    @abstractmethod
     def predict_proba(self, features_flat: Arr) -> Arr:
         raise NotImplementedError
 
     # TODO: make these polymorphic?
-    @abstractmethod
     def predict(self, features: Arr) -> UInt8Arr:
         h, w, c = features.shape
         features_flat = features.reshape((h * w, c))
@@ -33,7 +28,6 @@ class Classifier(object, metaclass=ABCMeta):
         seg_flat = np.argmax(probs, axis=-1)
         return seg_flat.reshape((h, w))
 
-    @abstractmethod
     def save(self, out_path: str, as_skops: bool = False) -> None:
         if as_skops is False:
             with open(out_path, "wb") as f:
