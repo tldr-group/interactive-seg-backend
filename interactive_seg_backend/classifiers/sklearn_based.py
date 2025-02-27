5b6925c9e6d9626bbfb0568af08c6ce485d9ab22
rmdocherty <rmdocherty2@gmai.com>
2025-02-27 12:40:41 +0000

    polymorphic RF

--- interactive_seg_backend/classifiers/sklearn_based.py
+++ interactive_seg_backend/classifiers/sklearn_based.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import RidgeClassifier, LogisticRegression
 
-from interactive_seg_backend.configs import Arr, UInt8Arr
+from interactive_seg_backend.configs import Arr, Arrlike, UInt8Arr
 from .base import Classifier
 
 from typing import Any
@@ -12,15 +12,18 @@ class RandomForest(Classifier):
         self.model = RandomForestClassifier(**extra_args)
 
     def fit(
-        self, train_data: Arr, target_data: UInt8Arr, sample_weights: Arr | None = None
+        self,
+        train_data: Arrlike,
+        target_data: UInt8Arr,
+        sample_weights: Arr | None = None,
     ):
         self.model.fit(train_data, target_data, sample_weight=sample_weights)
         return self
 
-    def predict_proba(self, features_flat: Arr) -> Arr:
+    def predict_proba(self, features_flat: Arrlike) -> Arr:
         return self.model.predict_proba(features_flat)
 
-    def predict(self, features: Arr):
+    def predict(self, features: Arrlike):
         return super().predict(features)
 
 
