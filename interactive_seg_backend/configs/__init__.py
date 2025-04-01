9f3ac0781ad5d0a51057fce3735c0346c9708cff
rmdocherty <rmdocherty2@gmai.com>
2025-04-01 14:39:35 +0100

    module update

--- interactive_seg_backend/configs/__init__.py
+++ interactive_seg_backend/configs/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     "FeatureConfig",
     "CRFParams",
     "TrainingConfig",
+    "load_training_config_json",
     "FloatArr",
     "UInt8Arr",
     "IntArr",
@@ -10,5 +11,5 @@ __all__ = [
     "Arrlike",
     "UInt8Arrlike",
 ]
-from .config import FeatureConfig, TrainingConfig, CRFParams
+from .config import FeatureConfig, TrainingConfig, CRFParams, load_training_config_json
 from .types import FloatArr, UInt8Arr, IntArr, Arr, AnyArr, Arrlike, UInt8Arrlike
