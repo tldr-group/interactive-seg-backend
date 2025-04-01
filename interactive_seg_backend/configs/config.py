a9d97175886506d6df4e0709da449f931b177fe5
rmdocherty <rmdocherty2@gmai.com>
2025-04-01 15:40:30 +0100

    added dino feature flag

--- interactive_seg_backend/configs/config.py
+++ interactive_seg_backend/configs/config.py
@@ -233,6 +233,7 @@ class TrainingConfig:
     autocontext: bool = False
     CRF: bool = False
     CRF_params: CRFParams = default_crf_params
+    add_dino_features: bool = False
 
     HITL: bool = False
     HITL_strategy: HITLStrategy = "wrong"
