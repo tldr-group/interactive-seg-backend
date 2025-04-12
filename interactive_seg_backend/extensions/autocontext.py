a6b1ee69bb2cc1782a11d187e40a78e068f91da4
rmdocherty <rmdocherty2@gmai.com>
2025-04-12 10:06:06 +0100

    allow option to supply precompute feats to ac

--- interactive_seg_backend/extensions/autocontext.py
+++ interactive_seg_backend/extensions/autocontext.py
@@ -16,16 +16,21 @@ def autocontext_features(
     image: Arr,
     labels: UInt8Arr,
     train_cfg: TrainingConfig,
+    original_feats: Arr | None = None,
     which: AutocontextType = "autocontext_ilastik",
 ) -> AnyArr:
-    original_feats = featurise_(
-        image, train_cfg.feature_config, train_cfg.preprocessing, train_cfg.use_gpu
-    )
-    _, probs, _ = train_and_apply_(original_feats, labels, train_cfg)
+    if original_feats is None:
+        feats = featurise_(
+            image, train_cfg.feature_config, train_cfg.preprocessing, train_cfg.use_gpu
+        )
+    else:
+        feats = original_feats
+
+    _, probs, _ = train_and_apply_(feats, labels, train_cfg)
     if which == "autocontext_original":
-        return concat_feats(original_feats, probs)
+        return concat_feats(feats, probs)
     else:
         prob_feats = featurise_(
             probs, train_cfg.feature_config, train_cfg.preprocessing, train_cfg.use_gpu
         )
-        return concat_feats(original_feats, prob_feats)
+        return concat_feats(feats, prob_feats)
