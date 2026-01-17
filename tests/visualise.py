49efb2d85299f4b1dc107aecf8b7d2506613ce73
rmdocherty <rmdocherty2@gmail.com>
2026-01-17 19:43:33 +0000

    fix vis script

--- tests/visualise.py
+++ tests/visualise.py
@@ -41,9 +41,7 @@ def plot_single_axis(
     )
 
 
-def setup_figure(
-    n_cols: int, n_rows: int = 2, add_diff_hist: bool = False
-) -> tuple[Figure, npt.NDArray[Any]]:
+def setup_figure(n_cols: int, n_rows: int = 2, add_diff_hist: bool = False) -> tuple[Figure, npt.NDArray[Any]]:
     axs: npt.NDArray[Any]
     if add_diff_hist:
         n_rows += 1
@@ -69,13 +67,13 @@ def plot(
     else:
         sample_inds = inds
     feat_names = [feat_names[ind] for ind in sample_inds]
+    print(feat_names)
 
     fig, axs = setup_figure(len(feat_names), add_diff_hist=add_diff)
 
-    for i, name in enumerate(feat_names):
-        arrs = (cpu_feats[:, :, i], gpu_feats[:, :, i])
-        vmin, vmax = np.amin(arrs[0]), np.amax(arrs[0])
-        # print(f"{name}: {vmin}, {vmax}")
+    for i, (feat_idx, name) in enumerate(zip(sample_inds, feat_names)):
+        arrs = (cpu_feats[:, :, feat_idx], gpu_feats[:, :, feat_idx])
+        # vmin, vmax = np.amin(arrs[0]), np.amax(arrs[0])
         for j, arr in enumerate(arrs):
             if i == 0 and j == 0:
                 label = labels[0]
@@ -94,7 +92,10 @@ def plot(
         if add_diff:
             h, w = arrs[0].shape
             diffs = np.abs((arrs[0] - arrs[1])).reshape(h * w)
-            axs[-1, i].hist(diffs, density=True, bins=100)
+            try:
+                axs[-1, i].hist(diffs, density=True, bins=100)
+            except ValueError:
+                pass
     plt.tight_layout()
     plt.savefig("tests/out/vis.png")
     plt.close()
@@ -114,12 +115,8 @@ if __name__ == "__main__":
     )
     feats_cpu = multiscale_features(DATA, cfg)
 
-    img_tensor = (
-        torch.tensor(DATA, device="cuda:0", dtype=torch.float32)
-        .unsqueeze(0)
-        .unsqueeze(0)
-    )
-    feats_gpu = multiscale_features_gpu(img_tensor, cfg, torch.float32)
+    img_tensor = torch.tensor(DATA, device="cuda:0", dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+    feats_gpu = multiscale_features_gpu(img_tensor, cfg)
     feats_gpu_np: np.ndarray = feats_gpu.cpu().numpy()
 
     plot(feats_cpu, feats_gpu_np, cfg, ADD_DIFF_HIST, 10)
