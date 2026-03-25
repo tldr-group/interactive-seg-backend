add connectivity obj, fix phase masking for vf obj

interactive_seg_backend/extensions/expertseg.py
@@ -7,13 +7,17 @@ authors: Nina Prakash, Paul Gasper, Francois Usseglio-Viretta
 Allows users to add domain-knowledge inspired losses to their XGBoost trainable segmentation.
 """
 
+import numpy as np
 from xgboost import DMatrix, Booster
 from sklearn.preprocessing import OneHotEncoder
-import numpy as np
+from scipy.ndimage import distance_transform_edt
+from skimage.measure import label, regionprops
 
 from typing import Any, cast
 from interactive_seg_backend.configs import ClassInfo, NPFloatArray, NPUIntArray
 from interactive_seg_backend.classifiers import XGBCPU
+from interactive_seg_backend.configs.types import ConnectivityObj
+from interactive_seg_backend.utils import logger
 
 
 class ExpertSegClassifier(XGBCPU):
@@ -62,7 +66,7 @@ class ExpertSegClassifier(XGBCPU):
             sample_weights (NPFloatArray | None, optional): _description_. Defaults to None.
 
         Returns:
-            _type_: _description_
+            self
         """
         ih, iw, c = train_data.shape
         lh, lw = target_data.shape
@@ -81,6 +85,12 @@ class ExpertSegClassifier(XGBCPU):
 
         n_classes = labels_onehot.shape[-1]
         targ_vfs = _get_target_vf_dist(n_classes, self.class_infos)
+        targ_conns = _get_target_connectivities(n_classes, self.class_infos)
+
+        if self.do_vf_loss:
+            logger.info(f"Target vfs: {targ_vfs}")
+        if self.do_conn_loss:
+            logger.info(f"Target connectivity: {targ_conns}")
 
         self.params["num_class"] = n_classes
         model = Booster(self.params, [train_dmat])
@@ -96,6 +106,11 @@ class ExpertSegClassifier(XGBCPU):
                 _, g_vf_full = volume_fraction_obj(full_img_pred, self.lambd_vf, targ_vfs)
                 g_vf = g_vf_full[label_mask[0]]
                 g += g_vf
+            if self.do_conn_loss:
+                full_img_pred_2D = full_img_pred.reshape((ih, iw, n_classes))
+                _, g_conn_full = connectivity_obj(full_img_pred_2D, self.lambd_conn, targ_conns)
+                g_conn = g_conn_full[label_mask[0]]
+                g += g_conn
 
             model.boost(train_dmat, i, grad=g, hess=h)
 
@@ -135,7 +150,7 @@ def volume_fraction_obj(
     pred_onehot = np.eye(n_classes)[np.argmax(whole_img_pred, axis=1)]
     pred_vf_distr = np.mean(pred_onehot, axis=0)
     # mask out classes with no target vf (i.e. where target_vf_distr is -1)
-    valid_vf_mask = np.where(target_vf_distr >= 0, 1, 0)
+    valid_vf_mask = np.where(target_vf_distr > 0, 1, 0)
     pred_vf_distr *= valid_vf_mask
     target_vf_distr *= valid_vf_mask
 
@@ -145,3 +160,79 @@ def volume_fraction_obj(
     grad = np.tile(grad_row, (n_px, 1))
     grad = cast(NPFloatArray, grad)
     return loss, grad
+
+
+def _get_target_connectivities(n_classes: int, class_infos: list[ClassInfo]) -> list[ConnectivityObj]:
+    targs: list[ConnectivityObj] = [None for i in range(n_classes)]
+    for info in class_infos:
+        phase_idx = info.value
+        targs[phase_idx] = info.connectivity_target
+    return targs
+
+
+def connectivity_obj(
+    whole_img_pred_2D: NPFloatArray, lambd: float, targets: list[ConnectivityObj]
+) -> tuple[float, NPFloatArray]:
+    ih, iw, n_classes = whole_img_pred_2D.shape
+    pred_labels: NPUIntArray = np.argmax(whole_img_pred_2D, axis=-1)
+
+    loss = 0.0
+    grad = np.zeros((ih * iw, n_classes))
+    for i, targ in enumerate(targets):
+        if targ is None:  # skip classes w/out connectivity target
+            continue
+
+        binary_pred = pred_labels == i
+        class_loss, class_grad = _per_class_connectivity_obj(binary_pred, lambd, targ)
+        class_grad = class_grad.reshape((ih * iw))
+
+        loss += class_loss
+        for j in range(len(targets)):
+            mult = 1 if j == i else -1
+            grad[:, j] += mult * class_grad
+    return loss, grad
+
+
+def _fast_uniform_check(binary_arr: np.ndarray) -> bool:
+    has_true = np.any(binary_arr)
+    has_false = not np.all(binary_arr)
+    if has_true and has_false:
+        return False
+    else:
+        return True
+
+
+def _per_class_connectivity_obj(
+    binary_pred: np.ndarray, lambd: float, targ: ConnectivityObj
+) -> tuple[float, NPFloatArray]:
+    is_uniform = _fast_uniform_check(binary_pred)
+    if is_uniform:  # early return if pred all 0s or all 1s (i.e defer to softmax)
+        return 0.0, np.zeros_like(binary_pred)
+
+    labels, n_components = label(binary_pred, return_num=True)
+
+    if targ == "minimise":
+        loss = lambd * (1 / n_components)
+        distance_map: NPFloatArray = distance_transform_edt(binary_pred, return_indices=False)
+        distance_map_inv = np.divide(1, distance_map, out=np.zeros(distance_map.shape), where=distance_map != 0)
+        grad_2D = lambd * distance_map_inv * loss
+        return loss, grad_2D
+    elif targ == "maximise":
+        loss = lambd * np.log(n_components)
+        props = regionprops(labels)
+        areas: list[int] = [prop.area for prop in props]
+        area_map = np.zeros_like(binary_pred, dtype=np.float32)
+        max_area = np.max(areas)
+        for prop in props:
+            area_map[labels == prop.label] = prop.area / max_area
+        gradient_area = lambd * area_map * loss
+
+        binary_inv = ~binary_pred
+        distance_map: NPFloatArray = distance_transform_edt(binary_inv, return_indices=False)
+        distance_map_inv = np.divide(1, distance_map, out=np.zeros(distance_map.shape), where=distance_map != 0)
+        grad_distance = lambd * distance_map_inv * loss
+
+        grad_2D = gradient_area + grad_distance
+        return loss, grad_2D
+    else:
+        raise Exception(f"Invalid target: {targ}")